289275a825bfafd06061db42554f30d4e37f0fb2
Stekl0 <gulev_s@list.ru>
2024-11-17 02:38:24 +0500

    kirill lox

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+import telebot
 from config import  BOT_KEY
 
 app = Flask(__name__, template_folder='./templates')
 
 
-@app.route('/')
+@app.route('/payment')
 def payment():  # put application's code here
     return render_template('payment.html')
+
+@app.route('/')
+def fee():  # put application's code here
+    return render_template('fee.html')
+
+@app.route('/finish')
+def finishPayment():  # put application's code here
+    return render_template('finishPayment.html')
 
 
 if __name__ == '__main__':
