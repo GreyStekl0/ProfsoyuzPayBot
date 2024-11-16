9038bc0fae3049a272eb9895abfd2af24aa4639b
Stekl0 <gulev_s@list.ru>
2024-11-16 20:21:22 +0500

    start

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from config import  BOT_KEY
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def payment():  # put application's code here
     return render_template('payment.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port='80')
